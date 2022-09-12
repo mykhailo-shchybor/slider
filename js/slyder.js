4326588efe8d7644e1b00dfe4c511ff957580905
Shchybor <migelito785@gmail.com>
2022-09-12 21:14:08 +0100

    Update

--- js/slyder.js
+++ js/slyder.js
@@ -1,6 +1,25 @@
-$('.autoplay').slick({
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
   slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
